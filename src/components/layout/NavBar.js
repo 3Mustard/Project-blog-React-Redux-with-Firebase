cd0e1433180e002ca5771c25392b35cc6b5f3432
3Mustard <m23.cullen@gmail.com>
2020-05-05 22:46:45 -0400

    first commit, created structure and started navbar.

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,15 @@
+//no state == functional component
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+const NavBar = () => {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <Link to='/' className="brand-logo">Twice</Link>
+            </div>
+        </nav>   
+    )
+}
+
+export default NavBar;
