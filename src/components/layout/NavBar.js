324927197c45dc30670777fbd5861ef3d84e28dd
3Mustard <m23.cullen@gmail.com>
2020-05-05 23:04:26 -0400

    TEST COMPONENT: SignedOutLinks | RESULT: PASS

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 const NavBar = () => {
     return (
@@ -9,6 +10,7 @@ const NavBar = () => {
             <div className="container">
                 <Link to='/' className="brand-logo">Twice</Link>
                 <SignedInLinks />
+                <SignedOutLinks />
             </div>
         </nav>   
     )
