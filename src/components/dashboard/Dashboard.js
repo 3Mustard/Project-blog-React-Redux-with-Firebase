fdd4af8a6395329882dbfbc49f91948fda09691a
3Mustard <m23.cullen@gmail.com>
2020-05-08 17:13:49 -0400

    Create Component: Notifications, Add Component Notifications to Component Dashboard.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,7 +7,9 @@ class Deashboard extends Component {
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6"></div>
-                    <div className="col s12 m5 offset-m1"></div>
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications />
+                    </div>
                 </div>
             </div>
         )
