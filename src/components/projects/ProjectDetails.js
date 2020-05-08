5f3c9bc7e27a70e1cd62889bfddbb076384d45ff
3Mustard <m23.cullen@gmail.com>
2020-05-08 19:57:06 -0400

    USE LAST COMMIT FOR STABLE. Trying to fix [HMR] Waiting for update signal from WDS...

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const ProjectDetails = (props) => {
-    console.log(props)
+    const id = props.match.params.id;
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project title</span>
+                    <span className="card-title">Project title - {id}</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in tellus mi. Proin id efficitur dui. Suspendisse imperdiet egestas nulla vitae mollis. Curabitur a orci dictum, vestibulum nulla quis, lacinia elit. Aenean interdum ipsum at libero commodo fringilla. Nam posuere ut ipsum sit amet ornare. Ut velit tellus, aliquet eget tristique sed, sagittis a odio. Aenean sagittis et nisi sit amet vulputate. Nulla id nibh fermentum, pulvinar felis in, malesuada metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vitae cursus tellus. Duis eget elit vel leo scelerisque tincidunt nec a massa. Maecenas odio lectus, consectetur in dolor eu, faucibus auctor sapien. Morbi rutrum hendrerit velit a suscipit.</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
