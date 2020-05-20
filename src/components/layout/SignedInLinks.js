830832dd7d56276f8a3f425d99e9e2346dfe01d5
Matt <m23.cullen@gmail.com>
2020-05-20 15:57:28 -0400

    changed some links to go to the correct route

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>New something</NavLink></li>
+            <li><NavLink to='/new/project'>new project</NavLink></li>
             <li><NavLink to='/'>log out</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>CC</NavLink></li>
         </ul>  
