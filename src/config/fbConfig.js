764510f694ab2d36187f9146fe61287c4f1b61c4
Matt <m23.cullen@gmail.com>
2020-05-22 18:01:28 -0400

    setup firebase config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA32zJe3geHTy0nyaP8GUUtWaEo7SZgy-o",
+    authDomain: "twice-51acf.firebaseapp.com",
+    databaseURL: "https://twice-51acf.firebaseio.com",
+    projectId: "twice-51acf",
+    storageBucket: "twice-51acf.appspot.com",
+    messagingSenderId: "1090242158652",
+    appId: "1:1090242158652:web:10c435a63f7900b5a2e339",
+    measurementId: "G-VWPRKZY65T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true});
+  firebase.analytics();
+
+  export default firebase;
