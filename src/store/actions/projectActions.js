10a8bef6a9758b209a972645e95a8aa13b891ac8
Matt <m23.cullen@gmail.com>
2020-05-23 13:01:28 -0400

    fleshed out the create project action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,16 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
-        dispatch({ type: 'CREATE_PROJECT', project });
-    }
-}
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project, 
+            authorFirstName: 'M',
+            authorLastName: 'C',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((error) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', error });
+        });
+    };
+};
