071dfcc2a4619c1c43f459df4d42dbb4f4576442
Matt <m23.cullen@gmail.com>
2020-05-25 19:57:33 -0400

    setting up auth

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,8 +1,8 @@
-//no state == functional component
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 const NavBar = () => {
     return (
@@ -16,4 +16,11 @@ const NavBar = () => {
     )
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(NavBar);
