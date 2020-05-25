8866be9d88a9cc37a26b192d6530bbe1fcec0e38
Matt <m23.cullen@gmail.com>
2020-05-25 14:19:27 -0400

    added if/else returns to rendering project details

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
 
 const ProjectDetails = (props) => {
-    const { project } = props;
-    if (project) {
-        return (
-            <div className="container section project-details">
-                <div className="card z-depth-0">
-                    <div className="card-content">
-                        <span className="card-title">{ project.title }</span>
-                        <p>{ project.content }</p>
-                    </div>
-                    <div className="card-action grey lighten-4 grey-text">
-                        <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
-                        <div className="grey-text">2nd September</div>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="container center">
-                <p>loading project...</p>
-            </div>
-        )
-    }
-} 
+  const {project} = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{ project.title }</span>
+            <p>{ project.content }</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
+            <div className="grey-text">2nd September</div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>loading project...</p>
+      </div>
+    );
+  }
+};
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        project: state.firestore.data.project 
-    }
-}
+  return {
+    project: state.firestore.data.project,
+  };
+};
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect((props) => {
-        return [
-             { collection: "projects", doc: props.match.params.id, storeAs: "project"}
-        ];
-    })
+      return [
+        {collection: 'projects', doc: props.match.params.id, storeAs: 'project'},
+      ];
+    }),
 )(ProjectDetails);
