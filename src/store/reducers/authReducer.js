1be4996d6d6a841b5b9fa77539a28dd431717e07
Matt <m23.cullen@gmail.com>
2020-05-26 00:18:13 -0400

    set up authreducer to logging in

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,25 @@
-const initialState = {}
+const initialState = {
+    authError: null
+}
 
 const authReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        //if this case happens, the state plus and error will go to the root reducer then store under the auth: key. the root reducer is our store based on the keys it has associated with reducers.
+        case 'LOGIN_ERROR':
+            consoles.log('login error');
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGIN_SUCCESS':
+            consoles.log('login success');
+            return {
+                ...state,
+                authError: null
+            }
+            default:
+                return state;
+    }
 }  
 
 export default authReducer;
