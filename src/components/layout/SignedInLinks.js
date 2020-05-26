722118ad613364e34d0dafe7979840810bc43d36
Matt <m23.cullen@gmail.com>
2020-05-26 13:26:49 -0400

    connected dispatch to signed in links, correctly signs a user out when clicked.

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'; 
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
+    console.log(props);
     return (
         <ul className="right">
             <li><NavLink to='/new/project'>new project</NavLink></li>
-            <li><NavLink to='/'>Sign out</NavLink></li>
+            <li><a onClick={props.signOut}>Sign out</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>CC</NavLink></li>
         </ul>  
     )
 }
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
