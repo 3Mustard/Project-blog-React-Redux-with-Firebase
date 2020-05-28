8722141f9c5ffa200a25366138089d7c09a1cc00
Matt <m23.cullen@gmail.com>
2020-05-27 22:36:33 -0400

    added missing import

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {compose} from 'redux';
 import {connect} from 'react-redux';
 import {firestoreConnect} from 'react-redux-firebase';
-import {compose} from 'redux';
+import { Redirect } from 'react-router-dom';
+
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
