17296b4f7c26ba2e888b4059b139e6be4d9d4665
Matt <m23.cullen@gmail.com>
2020-05-30 21:38:18 -0400

    remove initial state values from projectReducer. Tested out new firebase rules for crud actions

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,9 +1,4 @@
 const initialState = {
-    projects: [
-        {id: '0', title: 'title 1', content: 'lalala'},
-        {id: '1', title: 'title 2', content: 'weewoo'},
-        {id: '2', title: 'title 3', content: 'vvv'}
-    ]
 }
 
 const projectReducer = (state = initialState, action) => {
