a85afa343add215fdfa57f43093402a897179cae
3Mustard <m23.cullen@gmail.com>
2020-07-07 20:42:27 -0400

    added deleteProject action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -20,18 +20,12 @@ export const createProject = (project) => {
 export const deleteProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         const firestore = getFirestore();
-        const profile = getState().firebase.profile;
-        const authorId = getState().firebase.auth.uid; 
-        firestore.collection('projects').add({
-            ...project, 
-            authorFirstName: profile.firstName,
-            authorLastName: profile.lastName,
-            authorId: authorId,
-            createdAt: new Date()
-        }).then(() => {
-            dispatch({ type: 'CREATE_PROJECT', project });
+        const projectID = project.projectID; 
+        firestore.collection('projects').doc(projectID).delete()
+        .then(() => {
+            dispatch({ type: 'DELETE_PROJECT'});
         }).catch((error) => {
-            dispatch({ type: 'CREATE_PROJECT_ERROR', error });
+            dispatch({ type: 'DELETE_PROJECT_ERROR', error });
         });
     };
 };
