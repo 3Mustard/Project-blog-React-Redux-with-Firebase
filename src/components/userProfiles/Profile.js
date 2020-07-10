bf29bdcbc34cfa3b23b6b7f4b61903d785bd4f94
3Mustard <m23.cullen@gmail.com>
2020-07-09 20:40:07 -0400

    added code for getting the current users profile from store to props

--- src/components/userProfiles/Profile.js
+++ src/components/userProfiles/Profile.js
@@ -1,29 +1,21 @@
-//mimick project details component structure in reguards to using a url for finding a user instead of a project.
-//this component also needs to use the url of the users id to get all projects that they have made.
-
 import React from 'react';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
 import {firestoreConnect} from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
-import moment from 'moment';
 
 const Profile = (props) => {
-  const { project, auth, user } = props;
-  if (!auth.uid && (auth.uid !== user.id)) return <Redirect to='/signin'/>
+  const { auth, user } = props;
+  if (!auth.uid) return <Redirect to='/signin'/>
 
-  if (project) {
+  if (user) {
     return (
       <div className="container section project-details">
         <div className="card z-depth-0">
           <div className="card-content">
-            <span className="card-title">{ project.idol }</span>
-            <p>{ project.group }</p>
-            <p>{ project.content }</p>
+            <span className="card-title">profile stuff</span>
           </div>
           <div className="card-action grey lighten-4 grey-text">
-            <div>Posted by { project.authorFirstName } { project.authorLastName }</div>
-            <div className="grey-text">{ moment(project.createdAt.toDate()).calendar() }</div>
           </div>
         </div>
       </div>
@@ -31,7 +23,7 @@ const Profile = (props) => {
   } else {
     return (
       <div className="container center">
-        <p>loading project...</p>
+        <p>loading profile...</p>
       </div>
     );
   }
@@ -48,7 +40,7 @@ export default compose(
     connect(mapStateToProps),
     firestoreConnect((props) => {
       return [
-        {user: 'users', doc: props.match.params.id, storeAs: 'user'},
+        {user: 'users', doc: state.firebase.auth.uid, storeAs: 'user'},
       ];
     }),
 )(Profile);
