a420f866c5f2a119b899148ba5e4953763c2a0a7
3Mustard <m23.cullen@gmail.com>
2020-07-16 02:48:28 -0400

    finished messagelist component. needs messagepreview component built to work still

--- src/components/messaging /MessageList.js
+++ src/components/messaging /MessageList.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import MessagePreview from 
 
 const MessageList = ({messages}) => {
     return (
         <div className="container">
             <div className="row">
                 <div className="s12 m5">
-                    messages go here
+                    {messages && messages.map(message => {
+                        return (
+                        <Link to={'/message/' + message.id} key={message.id} >
+                            <MessagePreview message={message} />
+                        </Link>
+                        );
+                    })}
                 </div>
             </div>
         </div>
